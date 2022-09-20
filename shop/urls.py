ca8357f16cbd4e809a1e941c461bc58d97ede9a8
Boyko Alexey <elorian@yandex.ru>
2022-09-20 12:11:52 +0300

    Add product page

--- shop/urls.py
+++ shop/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name='index')
+    path('', views.index, name='index'),
+    path('item/<int:item_id>/', views.product_view, name='product_view')
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,14 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, get_object_or_404
+from .models import Item
+
 
 def index(request):
-    return HttpResponse('HOHO!')
+    items = Item.objects.all
+    context = {'items': items}
+    return render(request, 'index.html', context=context)
+
+
+def product_view(request, item_id):
+    item = get_object_or_404(Item, pk=item_id)
+    context = {'item': item}
+    return render(request, 'product.html', context=context)
